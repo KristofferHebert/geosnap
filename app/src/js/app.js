feb17b9a8a3c88ec45da345dae7afcc364d072ec
= <kristoffer@kristofferhebert.com>
2016-07-14 18:10:21 -0700

    feature: adding sw file

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import registerSW from './app/components/utils/registerSW'
 import App from './app/index'
+
+registerSW('/sw.js')
 
 ReactDOM.render(<App />, document.getElementById('react-mount'))
