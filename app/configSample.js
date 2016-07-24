03ac7285b8f442684bc058b39fba2fcd78f49a84
= <kristoffer@kristofferhebert.com>
2016-07-24 14:22:07 -0700

    refactor: changing port 443 for ssl

--- app/configSample.js
+++ app/configSample.js
@@ -1,7 +1,7 @@
 // rename to config.js with your own settings.
 
 const config = {
-  port: 8001,
+  port: 443,
   env: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
   keys: ['dfkjfkjhds45', 'skjhjkhdsfkjhdfs', '...'],
   database: (process.env.NODE_ENV === 'production') ? 'geosnap_production' : 'geosnap_staging'
